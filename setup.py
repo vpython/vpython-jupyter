ef5e2e3a78aca915f263c32658c55e9198add635
Matthew Craig <mattwcraig@gmail.com>
2018-10-07 18:02:55 -0500

    Restrict ipykernel version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except ImportError:
 
 import versioneer
 
-install_requires = ['jupyter', 'vpnotebook', 'numpy']
+install_requires = ['jupyter', 'vpnotebook', 'numpy', 'ipykernel<5']
 
 if sys.version_info.major == 3 and sys.version_info.minor >= 5:
     install_requires.append('autobahn')
