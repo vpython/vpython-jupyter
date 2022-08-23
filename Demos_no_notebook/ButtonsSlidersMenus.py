5598e7bc8f08c674c9b4fc075cf05bc5194a1312
BruceSherwood <bruce.sherwood@gmail.com>
2022-08-22 19:32:13 -0700

    Update ButtonsSlidersMenus

--- Demos_no_notebook/ButtonsSlidersMenus.py
+++ Demos_no_notebook/ButtonsSlidersMenus.py
@@ -1,6 +1,4 @@
 from vpython import *
-# This version uses VPython widgets: button, radio button, checkbox, slider, menu
-# See ButtonsSlidersMenus1 for a version that uses Jupyter notebook widgets: button, slider, menu
 scene.width = 350
 scene.height = 300
 scene.range = 1.3
@@ -20,7 +18,6 @@ box_object = box(visible=True)
 cone_object = cone(visible=False, radius=0.5)
 pyramid_object = pyramid(visible=False)
 cylinder_object = cylinder(visible=False, radius=0.5)
-sphere(radius=0.3)
 
 col = color.cyan
 currentobject = box_object
@@ -30,35 +27,49 @@ def Color(c):
     global col
     if col.equals(color.cyan): # change to red
         currentobject.color = col = color.red
+        c.text = "<b>Cyan</b>"
+        c.color = color.cyan
+        c.background = color.red
+        if c.name is None: # this is the top button
+            r1.checked = False
+            r2.checked = True
+    else:                      # change to cyan
+        currentobject.color = col = color.cyan
+        c.text = "<b>Red</b>"
+        c.color = color.red
+        c.background = color.cyan
+        if c.name is None: # this is the top button
+            r1.checked = True
+            r2.checked = False
+        
+def cc(c):
+    global col
+    if col.equals(color.cyan): # change to red:
+        currentobject.color = col = color.red
         cbutton.text = "<b>Cyan</b>"
-        cbutton.textcolor = color.cyan
+        cbutton.color = color.cyan
         cbutton.background = color.red
-        r1.checked = False
-        r2.checked = True
     else:                      # change to cyan
         currentobject.color = col = color.cyan
         cbutton.text = "<b>Red</b>"
-        cbutton.textcolor = color.red
+        cbutton.color = color.red
         cbutton.background = color.cyan
-        r1.checked = True
-        r2.checked = False
         
-cbutton = button(text='<b>Red</b>', textcolor=color.red, background=color.cyan, pos=scene.title_anchor, bind=Color)
+cbutton = button(text='<b>Red</b>', color=color.red, background=color.cyan, 
+            pos=scene.title_anchor, bind=Color, name=None)
 
-scene.caption = "Vary the rotation speed:\n"
-speed = 150
+scene.caption = "Vary the rotation speed: \n\n"
+
 def setspeed(s):
-    global speed
-    speed = s.value
-    wt.text = '{:1.0f}'.format(s.value)
+    wt.text = '{:1.2f}'.format(s.value)
     
-sl = slider(min=20, max=500, value=250, length=280, bind=setspeed, right=15)
+sl = slider(min=0.3, max=3, value=1.5, length=220, bind=setspeed, right=15)
 
-wt = wtext(text='{:1.0f}'.format(sl.value))
+wt = wtext(text='{:1.2f}'.format(sl.value))
 
-scene.append_to_caption('\n')
+scene.append_to_caption(' radians/s\n')
 
-r1 = radio(bind=Color, checked=True, text='Cyan')
+r1 = radio(bind=cc, checked=True, text='Cyan', name='rads')
 
 scene.append_to_caption('       ')
 
@@ -85,7 +96,7 @@ menu(choices=['Choose an object', 'box', 'cone', 'pyramid', 'cylinder'], index=0
 
 scene.append_to_caption('\n')
 
-r2 = radio(bind=Color, text='Red')
+r2 = radio(bind=cc, text='Red', name='rads')
 
 scene.append_to_caption('         ')
 
@@ -97,8 +108,8 @@ def transparency(b):
 
 checkbox(bind=transparency, text='Transparent')
 
+dt = 0.01
 while True:
-    rate(100)
+    rate(1/dt)
     if running:
-        currentobject.rotate(angle=speed*1e-4, axis=vector(0,1,0))
-
+        currentobject.rotate(angle=sl.value*dt, axis=vector(0,1,0))
